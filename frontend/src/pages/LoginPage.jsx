4a06571a50dc285367a14265866191010123bb9f
Gaurav Joshi <gj451026@gmail.com>
2022-11-10 11:59:41 +0530

    LoginPage completed

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import "../styling/LoginPage.scss"
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='LoginPage_container'>
+      <form >
+        <label >
+          Enter username: &nbsp;
+        <input type="text" />
+        </label>
+        <label >
+          Enter password: &nbsp;
+        <input type="password"  />
+        </label>
+        <input type="submit" />
+      </form>
+      <div className="LoginPage_signup-link">
+        <Link to="/signup"> <p>Sign Up</p> </Link> 
+      </div>
+    </div>
   )
 }
 
