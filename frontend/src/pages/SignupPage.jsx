fa074dcc45fc6a6e8fa83c22a174d33d1a88b37f
Gaurav Joshi <gj451026@gmail.com>
2022-11-11 09:34:50 +0530

    Protective Routes added

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
+    <div>
+      <h1>Signup Page</h1>
+      <p> <Link to="/login">Log In</Link> </p>
+    </div>
   )
 }
 
---
+++ frontend/src/utils/RequireAuth.jsx
@@ -0,0 +1,14 @@
+// import React, { useContext } from "react";
+// import { AuthContext } from "../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+const RequireAuth = () => {
+//   const { user } = useContext(AuthContext);
+  let auth = false;
+  if (!auth) {
+    return <Navigate to="/login" />;
+  }
+  return <Outlet />;
+};
+
+export default RequireAuth;
